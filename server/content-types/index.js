c60aa214e0464e28d2f1b972f349d807c830e2e2
osadavc <osadavidath@gmail.com>
2022-06-01 21:19:17 +0530

    🔧 Created Content Types

--- server/content-types/index.js
+++ server/content-types/index.js
@@ -1,3 +1,5 @@
-'use strict';
+"use strict";
+const newsletter = require("./newsletter");
+const subscribedUser = require("./subscribed-user");
 
-module.exports = {};
+module.exports = { newsletter, "subscribed-user": subscribedUser };
